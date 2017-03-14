38723d656b913049d35c0f39b56ca6684b22ecaf
DeBraid <de.braid@gmail.com>
2017-03-14 15:52:11 -0400

    better naming and org for scape-ncaa

--- scrape-ncaa.js
+++ scrape-ncaa.js
@@ -1,24 +1,23 @@
 var page = require('webpage').create();
 var fs = require('fs');
-var teams = require('./ncaa-teamnames.js');
-// var year = '2012-2013';
+// var teams = require('./ncaa-teamnames.js');
+var args = require('system').args;
 var counter = 0;
 
-function handle_page(){
-	var team = 'ohio-state';
-	// var years = [
-	// 	'2001-2002', '2002-2003', '2003-2004', '2004-2005', '2005-2006', '2006-2007', '2008-2009', 
-	// 	'2010-2011', '2011-2012', '2012-2013', '2013-2014', '2014-2015', '2015-2016', '2016-2017'
-	// ];
-	var years = ['2015-2016', '2016-2017']
+function getNCAAdata(){
+	var team = args[1]; // first arg passed via command line
+	var years = [
+		'2001-2002', '2002-2003', '2003-2004', '2004-2005', '2005-2006', '2006-2007', '2008-2009', 
+		'2010-2011', '2011-2012', '2012-2013', '2013-2014', '2014-2015', '2015-2016', '2016-2017'
+	];
 	var year = years[counter]
-	console.log('handle_page counter', counter);
-	console.log('handle_page team', team);
-	console.log('handle_page year', year);
+	console.log('getNCAAdata counter', counter);
+	console.log('getNCAAdata team', team);
+	console.log('getNCAAdata year', year);
 	var URI = 'http://www.uscho.com/stats/team/'+team+'/womens-hockey/'+year+'/';
 	
 	page.open(URI, function (status) {
-		console.log('Page status', status);
+		console.log('Page Status', status);
 		counter += 1
 		if (status === 'success') {
 			var output_path = 'output_data/'+team+'-'+year+'.tsv';
@@ -31,12 +30,12 @@ function handle_page(){
 				phantom.exit();
 			}
 			else {
-				setTimeout(handle_page(), 100);
+				setTimeout(getNCAAdata(), 100);
 			}
 		}
 		else {
 			output_path = 'output_data/error-'+team+'-'+year+'.tsv';
-			var content = ''
+			var content = 'Error Status: ' + status;
 			fs.write(output_path, content, 'w');
 			phantom.exit();	
 		}
@@ -44,5 +43,5 @@ function handle_page(){
 }
 
 if (counter === 0) {
-	handle_page()
+	getNCAAdata()
 }
