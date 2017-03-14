ab9631c2cb6ec3ea9e0fee58ebfc5a7cd0e83c21
DeBraid <de.braid@gmail.com>
2017-03-14 11:01:39 -0400

    saving as TSVs (which import nicely to excel)

--- scrape-ncaa.js
+++ scrape-ncaa.js
@@ -1,13 +1,14 @@
 var page = require('webpage').create();
 var fs = require('fs');
-var output_path = 'ncaa_content.html';
+var output_path = 'ncaa_content.tsv';
 var ncaa_url = 'http://www.uscho.com/stats/team/bemidji-state/womens-hockey/2016-2017/';
 
 page.open(ncaa_url, function (status) {
   console.log('Page status', status);
   // var content = page.content;
   var content = page.evaluate(function () {
-  	return document.getElementById('teamOA').innerHTML;
+  	// return document.getElementById('teamOA').innerHTML;
+  	return document.getElementById('teamOA').innerText;
   })
   // console.log('Page content', content);
   // page.render('ncaa.png');
