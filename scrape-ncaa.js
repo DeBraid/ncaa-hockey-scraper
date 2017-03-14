3597dbfc420e1a00c8e8034ac172d0d8138934f2
DeBraid <de.braid@gmail.com>
2017-03-14 10:32:22 -0400

    cleaned up fn, now grabbing the table

--- scrape-ncaa.js
+++ scrape-ncaa.js
@@ -5,7 +5,11 @@ var ncaa_url = 'http://www.uscho.com/stats/team/bemidji-state/womens-hockey/2016
 
 page.open(ncaa_url, function (status) {
   console.log('Page status', status);
-  var content = page.content;
+  // var content = page.content;
+  var content = page.evaluate(function () {
+  	return document.getElementById('teamOA').innerHTML;
+  })
+  // console.log('Page content', content);
   // page.render('ncaa.png');
   fs.write(output_path, content, 'w');
   phantom.exit();
