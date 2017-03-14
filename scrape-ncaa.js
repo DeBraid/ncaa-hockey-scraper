834e0f28c1762c185ab2c36def48ad11ce27605e
DeBraid <de.braid@gmail.com>
2017-03-14 15:07:52 -0400

    ohio-state 2001-2015

--- scrape-ncaa.js
+++ scrape-ncaa.js
@@ -5,7 +5,7 @@ var teams = require('./ncaa-teamnames.js');
 var counter = 0;
 
 function handle_page(){
-	var team = 'north-dakota';
+	var team = 'ohio-state';
 	var years = [
 		'2001-2002', '2002-2003', '2003-2004', '2004-2005', '2005-2006', '2006-2007', '2008-2009', 
 		'2010-2011', '2011-2012', '2012-2013', '2013-2014', '2014-2015', '2015-2016', '2016-2017'
@@ -34,7 +34,7 @@ function handle_page(){
 			}
 		}
 		else {
-			output_path = output_path = 'output_data/error-'+team+'-'+year+'.tsv';
+			output_path = 'output_data/error-'+team+'-'+year+'.tsv';
 			var content = ''
 			fs.write(output_path, content, 'w');
 			phantom.exit();	
