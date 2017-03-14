58e58ce8544b65b55575d3cb51d295f703d882d4
DeBraid <de.braid@gmail.com>
2017-03-14 12:39:05 -0400

    creating output data for wisconsin

--- scrape-ncaa.js
+++ scrape-ncaa.js
@@ -1,18 +1,28 @@
 var page = require('webpage').create();
 var fs = require('fs');
 var teams = require('./ncaa-teamnames.js');
-console.log('teams', teams);
 var team = 'wisconsin';
-var year = '2016-2017';
+// var year = '2016-2017';
+var year = '2013-2014';
+// var years = ['2014-2015', '2015-2016', '2016-2017'];
 var output_path = 'output_data/'+team+'-'+year+'.tsv';
 var URI = 'http://www.uscho.com/stats/team/'+team+'/womens-hockey/'+year+'/';
-
-page.open(URI, function (status) {
+var createTSV = function (status) {
   console.log('Page status', status);
-  var content = page.evaluate(function () {
-  	// outputs a TSV
-  	return document.getElementById('teamOA').innerText;
-  })
-  fs.write(output_path, content, 'w');
-  phantom.exit();
-});
+  if (status === 'success') {
+	  var content = page.evaluate(function () {
+	  	// outputs a TSV
+	  	return document.getElementById('teamOA').innerText;
+	  })
+	  fs.write(output_path, content, 'w');
+	  phantom.exit();
+  }
+  else {
+  	output_path = output_path = 'output_data/error-'+team+'-'+year+'.tsv';
+  	var content = ''
+	fs.write(output_path, content, 'w');
+	phantom.exit();	
+  }
+}
+
+page.open(URI, createTSV);
