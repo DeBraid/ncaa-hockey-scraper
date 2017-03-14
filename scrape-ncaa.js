7c80b97d4783707c5181cad1b509fa79ba85b00a
DeBraid <de.braid@gmail.com>
2017-03-14 13:51:31 -0400

    include all years

--- scrape-ncaa.js
+++ scrape-ncaa.js
@@ -5,8 +5,11 @@ var teams = require('./ncaa-teamnames.js');
 var counter = 0;
 
 function handle_page(){
-	var team = 'minnesota-duluth';
-	var years = ['2011-2012', '2012-2013', '2013-2014', '2014-2015', '2015-2016', '2016-2017'];
+	var team = 'north-dakota';
+	var years = [
+		'2001-2002', '2002-2003', '2003-2004', '2004-2005', '2005-2006', '2006-2007', '2008-2009', 
+		'2010-2011', '2011-2012', '2012-2013', '2013-2014', '2014-2015', '2015-2016', '2016-2017'
+	];
 	var year = years[counter]
 	console.log('handle_page counter', counter);
 	console.log('handle_page team', team);
